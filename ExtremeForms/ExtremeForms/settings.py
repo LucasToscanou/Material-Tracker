53a3a1c632217ee9588be91e34aa4e19956ad901
lucas_swift <lucastoscanop@gmail.com>
2024-11-07 00:53:48 -0300

    deploy ready settings

--- ExtremeForms/ExtremeForms/settings.py
+++ ExtremeForms/ExtremeForms/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = '/var/www/html/static/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,6 +124,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/html/media/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "/static"),
